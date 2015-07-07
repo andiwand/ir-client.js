76cd1ae495bf568ffecb703d4d3894c469d9c4b5
andiwand <stefl.andreas@gmail.com>
2015-07-07 13:26:17 +0200

    add remote observer

--- src/ir/remote/remote.js
+++ src/ir/remote/remote.js
@@ -4,9 +4,28 @@ IR.remote = IR.remote || {};
 
 $.extend(IR.remote, {
   data: null,
-  load: function() {
-    IR.remote.data = IR.interface.load();
+  _ready: false,
+  _observers: [],
+  init: function() {
+    IR.interface.load(IR.remote._loaded);
+  },
+  isReady: function() {
+    return IR.remote._ready;
+  },
+  _fire: function() {
+    for(var i = 0; i < IR.remote._observers.length; i++) {
+      IR.remote._observers[i]();
+    }
+  },
+  addObserver: function(observer) {
+    if (IR.remote._ready) observer();
+    IR.remote._observers.push(observer);
+  },
+  _loaded: function(data) {
     // TODO: verify data
+    IR.remote.data = data;
+    IR.remote._ready = true;
+    IR.remote._fire();
   },
   save: function() {
     IR.interface.save(data);
